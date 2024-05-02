04b699ca426c37b4a2cf1bd749025934933f1a66
manuelfire <manuelandy@gmail.com>
2024-05-02 11:42:28 -0600

    go

---
+++ WebAPITests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod()]
+        public void GetTest2()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod()]
+        public void GetTest3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
